3909fdd21b6708235cf9ae8a788da63343f53111
Rahul Jain <rahul.jain@w3villa.com>
2023-04-25 18:06:11 +0530

    remove some extra import component they are not used also craete the some part of video Details page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 
 
 const Navbar = () =>  (
-    <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: "black", top: 0, justifyContent: "space-between"}} >
+    <Stack direction="row" alignItems="center" zIndex='10' p={2} sx={{ position: "sticky", background: "black", top: 0, justifyContent: "space-between"}} >
         <Link to="/" style={{ display: "flex", alignItems:"center"}}>
             <img src={logo}  alt="logo" height={45} />
         </Link>
