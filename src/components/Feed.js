11a667c0bd1bef1581a1f72031f421e2955b1531
Rahul Jain <rahul.jain@w3villa.com>
2023-04-25 11:43:38 +0530

    compleled video card component and also solve api error

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,23 +2,34 @@ import React, { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import Sidebar from './Sidebar'
 import Videos from './Videos'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 
 const Feed = () => {
+    const [ selectedCategory, setSelectedCategory] = useState('New')
+    const [videos, setVideos] = useState([]);
+
+    useEffect(()=>{
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data)=> setVideos(data.items))
+    },[selectedCategory]);
+    
   return (
     <Stack sx={{flexDirection: {sx: "column", md: "row"}}}>
         <div></div>
         <Box sx={{height: {sx:'auto', md:'92vh', borderRight:"1px solid #3d3d3d", px:{sx:0, md:2} }}}>
-            <Sidebar />
+            <Sidebar 
+                selectedCategory = {selectedCategory}
+                setSelectedCategory = {setSelectedCategory}
+            />
             <Typography variant="body2">
                 Copyright 2023 By Rahul Jain
             </Typography>
         </Box>
         <Box p={2} sx={{overflowY: "auto", height: "90vh", flex:2}}>
             <Typography variant="h4" fontWeight="bold" mb={2}>
-                New <span style={{color: "red"}}>Videos</span>
+            {selectedCategory} <span style={{color: "red"}}>Videos</span>
             </Typography>
-            <Videos videos={[]} />
+            <Videos videos={videos} />
         </Box>
     </Stack>
   )
---
+++ src/components/VideoCard.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia, sliderClasses } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import { demoThumbnailUrl, demoVideoUrl, demoChannelTitle, demoVideoTitle, demoProfilePicture } from '../utils/constants'
+
+const VideoCard = ({ video: { id:{videoId}, snippet } }) => {
+  return (
+    <Card sx={{width: {md:'320px', sx:'100%'}}}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl }>
+        <CardMedia
+          alt={snippet?.title}
+          sx={{height: 180, width: 368}}
+          image={snippet?.thumbnails?.high?.url } />
+      </Link>
+      <CardContent sx={{backgroundColor: '#1e1e1e', minHeight: '100px'}} >
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl }>
+          <Typography>
+            {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+          </Typography>
+        </Link> 
+        <Link to={snippet.channelId ? `/channel/${snippet.channelId}` : demoVideoUrl }>
+          <Typography>
+            {snippet?.channelTitle.slice(0,60) || demoChannelTitle.slice(0,60)}
+            <CheckCircle sx={{fontSize: '12px', ml:1}} />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+
+  )
+}
+
+export default VideoCard
