1bdf9f03db5826c43487584de09feec48570abb1
Rahul Jain <rahul.jain@w3villa.com>
2023-04-25 16:49:33 +0530

    add video search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,21 @@ import { Paper, IconButton } from '@mui/material'
 
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`)
+      setSearchTerm('')
+    }
+  }
+
   return (
-    <Paper component="form" onSubmit={()=>{}} 
+    <Paper component="form" onSubmit={handleSubmit} 
     sx={{borderRadius:20, border: "1px solid #e3e3e3", pl:2, boxShadow: "none", overflow:"hidden"  }}>
-        <input className='search-bar' placeholder='Search Bar' value=""  onChange={()=>{}} />
+        <input className='search-bar' placeholder='Search Bar' value={searchTerm}  onChange={(e)=>setSearchTerm(e.target.value)} />
         <IconButton type="submit" sx={{p:'5px' , color: "red"}}>
             <i className="fa-solid fa-magnifying-glass"></i>
         </IconButton>
